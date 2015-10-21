221b97485ce29380c80490c7ddf16e9f9da8005a
athoen <andrea.thoen@gmail.com>
2015-10-21 09:35:13 -0400

    push

---
+++ machinemain.js
@@ -0,0 +1,27 @@
+$(".knob").click(function(){
+    $(".talk").toggle();
+})
+
+$(".knob").on("click",function(e){ 
+$(".mouth").toggle();
+})
+
+$(".knob").on("click",function(e){ 
+$(".knob").addClass("green");
+})
+
+$(".door").on("click",function(){ 
+$(".foot").addClass("move")
+})
+
+$(".head").on("click",function(e){ 
+$(".head").addClass("bigger");
+})
+
+
+
+
+
+
+
+
