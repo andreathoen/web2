ec27a04a36adcdd2c3a6237a2eb6968dac39a18e
athoen <andrea.thoen@gmail.com>
2015-12-07 08:37:11 -0500

    burgees

---
+++ collection/class.js
@@ -0,0 +1,120 @@
+function showModal(item) { //show modal popout
+	$(".modal").empty(); //empty modal
+   
+	//identify the h1 by item.name
+   var $img = $("<img>").attr("src",item.imageURL);
+   //identify the img by item.image
+   var $h1 = $("<h3>").text(item.name);
+   var $p = $("<p>").text(item.Description);//identify the bodytext by item.description
+
+   $(".modal").append($h1,$img,$p);//adding the content into the modal popout
+   
+   $(".overlay").show(); //show grayed out background when img is clicked on
+   $(".modal").show();//show modal popout when img is clicked
+
+}
+
+function hideModal() {//display none modal when overlay in clicked again
+  $(".overlay").hide();
+  $(".modal").hide();
+}
+
+function addItem(item) {
+	
+	/*
+	var $li = $("<li>").addClass("item");//creating a list element with a class of item and a color
+	
+
+	$burgee.attr("data-category",item.affiliation); //adding an attribute of data category to the item.color
+	$li.attr("data-region",item.location);
+	
+	var $h2 = $("<h2 class='name'>").text(item.name);//creating a a h2 element 
+	var $p = $("<p class='description'>").text(item.established);//creating description to go with data
+
+	$li.append($h2,$p); //adding the h2 and description 
+*/
+	
+	var $burgee=$('<img>').attr("src",item.imageURL).addClass("item")
+	
+	$burgee.attr("data-location",item.location);
+	$burgee.attr("data-category",item.affiliation); //adding an attribute of data category to the item.color
+	
+  $burgee.on("click",function(e) {//on click of the image show the modal
+      showModal(item);
+  });
+
+  $(".collection").append($burgee);//adding the collection to the li elemetns
+};
+
+function addItems(data) {//identifying that the different items are part of the whole data
+  for(var i=0;i<data.length;i++) {
+    addItem(data[i]);
+  }
+}
+
+
+function callback(data) { 
+
+  addItems(data);
+
+  var $container = $('.collection');//selecting the containers to filter through??
+  $container.packery({
+    itemSelector: '.item',
+    gutter: 10,
+  });
+
+  var myCollection = $(".item").collection({//identify filters by the h2 and data-category input
+    filters: { 
+      "title": "h2",
+      "category": "[data-category]",
+	    "location": "[data-location]"
+    },
+
+    update: function() { //timing of filter through animation of packery?
+      $container.packery(); 
+      setTimeout(function() { $container.packery()},600);
+    },
+    ordered: function() {
+	    $container.packery('reloadItems');//calling the reload of packery when filtered through 
+      console.log("Whoa");
+    },
+    hide: function($elem) { $elem.hide(); },//hide the data that doesnt apply
+    show: function($elem) { $elem.fadeIn(500); },//show the data that does
+  });
+
+  $(".category").on("click",function(e) {//applying the toggles of color buttons
+      var category = $(this).data("category");
+      $("#filter-bar button.category").removeClass("selected");
+      $(this).addClass("selected");
+      myCollection.filtered("category",category);
+  });
+
+  $(".location-button").on("click",function(e) {//applying the toggles of color buttons
+      var category = $(this).data("location");
+      $("#filter-bar .location-button").removeClass("selected");
+      $(this).addClass("selected");
+      myCollection.filtered("location",category);
+  });
+
+
+  $(".order").on("click",function(e) {//applying the filters of ordering by
+      var order = $(this).data("order");
+      $("#filter-bar button.category").removeClass("selected");
+      $(this).addClass("selected");
+      myCollection.ordered(order);
+  });
+
+  $("#search").on("change keyup",function(e) {//as it searches look for the title (h1)
+      myCollection.filtered("title", $(this).val());
+  });
+
+  $(".overlay").on("click",function(e) {//on click of overlay hide modal
+      hideModal();
+  });
+
+}
+
+Tabletop.init( { key: "https://docs.google.com/spreadsheets/d/1sIAZ7KRvL8dXUzCowxeVPK4dUt9Lfm_NHFpTq7y2z5Q/pubhtml",
+                   callback: callback,
+                   simpleSheet: true } )
+ 
---
+++ collection/collection.js
@@ -0,0 +1,3 @@
+Tabletop.init( { key: "https://docs.google.com/spreadsheets/d/1sIAZ7KRvL8dXUzCowxeVPK4dUt9Lfm_NHFpTq7y2z5Q/edit#gid=0&vpid=A1",
+                 callback: callback,
+                 simpleSheet: true } )
